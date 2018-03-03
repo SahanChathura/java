7d2adda1498c73bbbe9f440c8ad0ab98917b3121
SahanChathura <sahanchathura140@gmail.com>
2018-03-03 12:06:19 +0530

    print the user input

---
+++ src/com/sahan/example/Two.java
@@ -0,0 +1,11 @@
+package com.sahan.example;
+
+import java.util.Scanner;
+public class Two {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number : ");
+		int number = sc.nextInt();
+		System.out.println("number is : "+number);
+	}
+}
